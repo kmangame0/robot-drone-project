1a9af312b009c083a9bf838847ce29d6b9a30ef0
Kellen Reason <kmangame0@gmail.com>
2016-10-27 15:04:34 -0500

    Added instructions and modified ps_drone

--- BasicMovement.py
+++ BasicMovement.py
@@ -1,28 +1,62 @@
 import time
 import ps_drone                # Imports the PS-Drone-API
-
 drone = ps_drone.Drone()       # Initializes the PS-Drone-API
 drone.startup()                # Connects to the drone and starts subprocesses
-
+time.sleep(1)
+drone.reset()
+NDC = drone.NavDataCount
 drone.takeoff()                # Drone starts
-time.sleep(7.5)                # Gives the drone time to start
+time.sleep(10)                # Gives the drone time to start
 
+landed = False
+drone.setSpeed(0.1)            # Sets default moving speed to 1.0 (=100%)
+drone.useDemoMode(False)
 ##drone.moveForward()            # Drone flies forward...
-##time.sleep(2)                  # ... for two seconds
+##time.sleep(1)                  # ... for two seconds
 ##drone.stop()                   # Drone stops...
 ##time.sleep(2)                  # ... needs, like a car, time to stop
-##
-##drone.moveBackward(0.25)       # Drone flies backward with a quarter speed...
-##time.sleep(1.5)                # ... for one and a half seconds
-##drone.stop()                   # Drone stops
-##time.sleep(2)	
 
-drone.setSpeed(0.1)            # Sets default moving speed to 1.0 (=100%)
-print drone.setSpeed()         # Shows the default moving speed
+while landed == False:
+    while drone.NavDataCount==NDC:      time.sleep(0.001)
+    print landed
+    try:
+        drone.getNDpackage(["demo","pressure_raw","altitude","magneto","wifi"])
+        #print "Altitude: "+str(abs(drone.NavData["altitude"][3]))
+        alt = abs(drone.NavData["altitude"][3])
+        if alt > 200 : ##Altitude in cm
+            drone.moveDown(10)
+            drone.hover()
+        else:
+            landed = True
+            drone.land()
+    except:
+        print "Corrupt Data"
+        continue
+
+##
+##drone.moveBackward()       
+##time.sleep(1)                
+##drone.stop()                   
+##time.sleep(2)	
+##
+##drone.moveLeft()       
+##time.sleep(1)                
+##drone.stop()                   
+##time.sleep(2)
+##
+##drone.moveRight()       
+##time.sleep(2)                
+##drone.stop()                   
+##time.sleep(2)
+##
+##drone.hover()
+##time.sleep(1)
+
+##print drone.setSpeed()         # Shows the default moving speed
 
 ##drone.turnLeft()               # Drone moves full speed to the left...
 ##time.sleep(2)                  # ... for two seconds
 ##drone.stop()                   # Drone stops
 ##time.sleep(2)
-time.sleep(10)
-drone.land()                   # Drone lands
+
+##drone.land()                   # Drone lands
