542faa387de2bf018b0319e679771c4bc9f9255f
Kellen Reason <kmangame0@gmail.com>
2016-10-28 13:25:44 -0500

    Much wow, edited code

--- WorkingLanding2.py
+++ WorkingLanding2.py
@@ -10,11 +10,34 @@ drone.reset()                                                                 # 
 while (drone.getBattery()[0] == -1):  time.sleep(0.1)                         # Wait the drone
 print "Battery: "+str(drone.getBattery()[0])+"%  "+str(drone.getBattery()[1]) # Gives a battery-status
 drone.useDemoMode(False)
+drone.setSpeed(0.2)
+time.sleep(0.2)
+
+drone.getNDpackage(["demo"])  
+firstYaw = drone.NavData["demo"][2][2]
+
+print str(firstYaw)
+
+time.sleep(0.1)
 drone.takeoff()
-time.sleep(12)
+time.sleep(15)
 forward = True
 land = False
-drone.setSpeed(0.2) 
+
+drone.getNDpackage(["demo"])  
+secondYaw = drone.NavData["demo"][2][2]
+firstYaw = firstYaw + secondYaw
+
+while abs(secondYaw-firstYaw)> 2.5:
+    print abs(secondYaw-firstYaw)
+    drone.getNDpackage(["demo"])  
+    secondYaw = drone.NavData["demo"][2][2]
+
+    if secondYaw > firstYaw:
+        drone.turnAngle(-2,0.25)
+    else:
+        drone.turnAngle(2,0.25)
+
 time.sleep(0.5)
 drone.hover()
 time.sleep(1)
@@ -24,26 +47,95 @@ time.sleep(1)
 drone.stop()
 time.sleep(2)
 
+while abs(secondYaw-firstYaw)> 2.5:
+    print abs(secondYaw-firstYaw)
+    drone.getNDpackage(["demo"])  
+    secondYaw = drone.NavData["demo"][2][2]
+
+    if secondYaw > firstYaw:
+        drone.turnAngle(-2,0.25)
+    else:
+        drone.turnAngle(2,0.25)
+        
+time.sleep(0.5)
+drone.hover()
+time.sleep(1)
+
 drone.moveForward()
 time.sleep(1)
 drone.stop()
 time.sleep(2)
+
+while abs(secondYaw-firstYaw)> 2.5:
+    print abs(secondYaw-firstYaw)
+    drone.getNDpackage(["demo"])  
+    secondYaw = drone.NavData["demo"][2][2]
+
+    if secondYaw > firstYaw:
+        drone.turnAngle(-2,0.25)
+    else:
+        drone.turnAngle(2,0.25)
+
+time.sleep(0.5)
+drone.hover()
+time.sleep(1)
 
 drone.moveRight()
 time.sleep(1)
 drone.stop()
 time.sleep(2)
 
+while abs(secondYaw-firstYaw)> 2.5:
+    print abs(secondYaw-firstYaw)
+    drone.getNDpackage(["demo"])  
+    secondYaw = drone.NavData["demo"][2][2]
+
+    if secondYaw > firstYaw:
+        drone.turnAngle(-2,0.25)
+    else:
+        drone.turnAngle(2,0.25)
+
+time.sleep(0.5)
+drone.hover()
+time.sleep(1)
+
 drone.moveBackward()
 time.sleep(1)
 drone.stop()
 time.sleep(2)
+
+while abs(secondYaw-firstYaw)> 2.5:
+    print abs(secondYaw-firstYaw)
+    drone.getNDpackage(["demo"])  
+    secondYaw = drone.NavData["demo"][2][2]
+
+    if secondYaw > firstYaw:
+        drone.turnAngle(-2,0.25)
+    else:
+        drone.turnAngle(2,0.25)
+
+time.sleep(0.5)
+drone.hover()
+time.sleep(1)
 
 drone.moveLeft()
 time.sleep(1)
 drone.stop()
 time.sleep(2)
 
+while abs(secondYaw-firstYaw)> 2.5:
+    print abs(secondYaw-firstYaw)
+    drone.getNDpackage(["demo"])  
+    secondYaw = drone.NavData["demo"][2][2]
+
+    if secondYaw > firstYaw:
+        drone.turnAngle(-2,0.25)
+    else:
+        drone.turnAngle(2,0.25)
+
+time.sleep(0.5)
+drone.hover()
+time.sleep(1)
 
 while land == False:
     drone.getNDpackage(["demo","pressure_raw","altitude","magneto","wifi"])       # Info needed
